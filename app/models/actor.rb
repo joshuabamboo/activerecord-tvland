3a5dcdb89c9535bf4fa87c255e8b24c91a4ddc8a
joshuabamboo <joshuaowens011@gmail.com>
2015-08-21 11:02:58 -0400

    complete review

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    # "Emilia Clarke"
+    "#{self.first_name} #{last_name}"
+  end
+
+  def list_roles
+    # "Khaleesi - Game of Thrones"
+    characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end.join(" ")
+
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,34 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  ALL_CHARACTERS = []
+
+  def say_that_thing_you_say
+     # "Steve Urkel always says: Did I do that?"
+     "#{name} always says: #{catchphrase}"
+  end
+
+  def self.add_to_all_character_list
+    
+  end
 end
+
+Character.new.say_that_thing_you_say
+
+khaleesi = Character.new
+khaleesi.hair = "brown"
+khaleesi.eyes = "blue"
+khaleesi.say_that_thing_you_say
+
+
+khalees = Character.new
+khalei = Character.new
+khesi = Character.new
+kheesi = Character.new
+khaleesi = Character.new
+khaleesi = Character.new
+
+Character.add_to_all_character_list
+
+khaleesi.say_that_thing_you_say
